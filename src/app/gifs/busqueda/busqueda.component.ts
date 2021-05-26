fdf9a73f047dc098fce3c3a4607a498a8152eda4
Paula Sierra <PaulaCode01@gmail.com>
2021-05-26 09:48:14 -0500

    Modulo shared

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent implements OnInit {
+
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>; //creamos el viewchild e utilizamos el htmlinput para tener las propiedades de input
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value='';
+  }
+}
